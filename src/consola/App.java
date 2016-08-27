eaadcf9e601c52dc7cf566e72d50ee51580bb261
Lupercar <hotolete@gmail.com>
2016-08-27 12:08:58 +0200

    Creo File y miro si es una carpeta o directorio

---
+++ src/consola/App.java
@@ -0,0 +1,29 @@
+package consola;
+
+import java.io.File;
+
+public class App {
+	
+
+	public static void main(String[] args) {
+		/*
+		 * La clase File representa una ruta del SO, 
+		 * esta clase nos abstrae del SO que estemos utilizando.  
+		 */
+		
+		//File ruta1 = new File("C:\\windows"); //Hay que escapar la "\" SO Windows sólo
+		File ruta1 = new File("C:/windows");  	//Es la manera recomendada "/" 
+		
+//		Operador ternario para comprobar si es una carpeta
+//		String resultado; 
+//		resultado = (ruta1.isDirectory()) ? "Es una carpeta" : "No es una carpeta"; 
+//		System.out.println(resultado);
+		
+//		Mediante un if
+		if(ruta1.isDirectory()){
+			System.out.println("Es una carpeta");
+		}else{
+			System.out.println("No es una carpeta");
+		}
+	}
+}//fin class consola.App
